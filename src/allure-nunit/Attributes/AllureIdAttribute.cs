d4bad5fc7fa5372356c0d4226c361031588f73cf
Aleksandr Kulakov <a.kulakov@dodopizza.com>
2022-08-26 13:43:37 +0500

    add additional attributes

---
+++ src/allure-nunit/Attributes/AllureIdAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Allure.Commons;
+
+namespace NUnit.Allure.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AllureIdAttribute : AllureTestCaseAttribute
+{
+    public AllureIdAttribute(string id)
+    {
+        Id = id;
+    }
+
+    private string Id { get; }
+
+    public override void UpdateTestResult(TestResult testResult)
+    {
+        testResult.labels.Add(new Label{ name = "AS_ID", value = Id });
+    }
+}
---
+++ src/allure-nunit/Attributes/AllureSuitesAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Allure.Commons;
+
+namespace NUnit.Allure.Attributes;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class AllureSuitesAttribute : AllureTestCaseAttribute
+{
+	public AllureSuitesAttribute(params string[] suites) : this("Suite", suites)
+	{ }
+
+	public AllureSuitesAttribute(string prefix, params string[] suites)
+	{
+		Suites = suites;
+		Prefix = prefix;
+	}
+		
+	private string[] Suites { get; }
+	private string Prefix { get; }
+
+	public override void UpdateTestResult(TestResult testResult)
+	{
+		for (var i = 0; i < Suites.Length; i++)
+		{
+			testResult.labels.Add(new Label{name = $"{Prefix}{i + 1}", value = Suites[i]});
+		}
+	}
+}
